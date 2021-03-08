e331a12acc9712b3d8bd5cab20e44890c8787903
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-03-07 20:12:38 -0600

    added resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+import resume from '../../assests/EFRAIN_NEVAREZJR_Resume_Final.PDF'
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+const Resume = () => {
+  const [numPages, setNumPages] = useState(null);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  return (
+    <Document
+      file={resume}
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      {Array.from(
+        new Array(numPages),
+        (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+          />
+        ),
+      )}
+    </Document>
+  )
+}
+export default Resume;
