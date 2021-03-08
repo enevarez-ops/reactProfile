2c49d7f58005199b6a19df26774e1a280a0333b2
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-03-07 20:18:48 -0600

    edited footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav'
+import Resume from './components/Resume'
+import Footer from './components/Footer'
 
 const App = () => {
   document.title = "Efrain Nevarez"
   return (
     // <Router>
-      <Nav ></Nav>
+
+    <>
+      <Nav />
+      <Footer />
+
+
+
+      </> 
+
+
     //   <Switch>
     //     <Route exact path='/portfolio'>
     //       <Home />
